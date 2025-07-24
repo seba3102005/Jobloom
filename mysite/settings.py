5d504d16dd1a47c5e9e2c0331db2046cc9da473e
seba ibraheem <sebaalmahdy@gmail.com>
2025-07-25 02:54:55 +0300

    static problem were solved  python manage.py collectstatic --clear --noinput

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')   ###joining the front_end with the back_end
